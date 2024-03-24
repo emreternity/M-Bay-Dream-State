e698aefde9b55ead6276699bb0675f9018c3f88d
HelofEternity <akiramotogami13@gmail.com>
2024-03-24 22:22:15 +0300

    proto1

--- test.py
+++ test.py
@@ -1,8 +1,10 @@
 # Example file showing a circle moving on screen
 import pygame
+import random
 
 # pygame setup
 pygame.init()
+pygame.mixer.init()
 pygame.display.set_caption("M-Bay: Dream State Prototip 1")
 screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
@@ -16,12 +18,12 @@ rect_char = pygame.Rect(0,0,100,100)
 npc = pygame.image.load('img\char\Lisa1.png')
 door = pygame.image.load('img\obj\door.png')
 door = pygame.transform.scale(door,(120,170))
-rect_door = pygame.Rect(0,0,140,190)
-rect_door.top = 140
-rect_door.left = 790
-rect_doorstop = pygame.Rect(0,0,140,95)
-rect_doorstop.top = 140
-rect_doorstop.left = 790
+rect_door = pygame.Rect(0,0,130,100)
+rect_door.top = 0
+rect_door.left = 610
+rect_doorstop = pygame.Rect(0,0,200,75)
+rect_doorstop.top = 0
+rect_doorstop.left = 570
 rect_npc = pygame.Rect(0,0,20,20)
 rect_npc.x = 360
 rect_npc.y = 210
@@ -33,9 +35,14 @@ isGamePaused = False
 chardown = [pygame.image.load('img\char\WillieIdle.png'),pygame.image.load('img\char\Willie1.png'),pygame.image.load('img\char\Willie2.png')]
 charright = [pygame.image.load('img\char\Willie-yan1.png'),pygame.image.load('img\char\Willie-yan2.png'),pygame.image.load('img\char\Willie-yan4.png')]
 charleft = [pygame.image.load('img\char\Willie-sol-yan1.png'),pygame.image.load('img\char\Willie-sol-yan2.png'),pygame.image.load('img\char\Willie-sol-yan4.png')]
-charup = [pygame.image.load('img\char\willie-arka-sonn.png'),pygame.image.load('img\char\willie-arka-sonn.png'),pygame.image.load('img\char\willie-arka-sonn.png')]
-
+charup = [pygame.image.load('img\char\williearka1.png'),pygame.image.load('img\char\williearka2.png'),pygame.image.load('img\char\williearka3.png')]
+charwalkongrass = [pygame.mixer.Sound('sfx\player\walk1.mp3'),pygame.mixer.Sound('sfx\player\walk2.mp3'),pygame.mixer.Sound('sfx\player\walk3.mp3'),pygame.mixer.Sound('sfx\player\walk4.mp3'),pygame.mixer.Sound('sfx\player\walk5.mp3'),pygame.mixer.Sound('sfx\player\walk6.mp3'),pygame.mixer.Sound('sfx\player\walk7.mp3')]
 font = pygame.font.Font('freesansbold.ttf',24)
+escmenusfx = pygame.mixer.Sound('sfx\enpc\dialogueblip.mp3')
+lisatalksfx = [pygame.mixer.Sound('sfx\enpc\lisatalk1.mp3'),pygame.mixer.Sound('sfx\enpc\lisatalk2.mp3')]
+act0song = pygame.mixer.Sound('sfx\embience\eact0routine.mp3')
+
+bg = pygame.image.load('img\proto1\proto1.png').convert()
 
 snip = font.render('', True, 'white')
 txtnpccounter = 0
@@ -45,8 +52,21 @@ txtnpcactivemsg = 0
 txtdoorcounter = 0
 txtdoorspeed = 2
 txtdoordone = False
+walksfxCounter = 10
+blipcounter = 0
+songcounter = 0
+
+pygame.mixer.music.load('sfx\embience\proto1ambience.mp3')
+pygame.mixer.music.play(-1)
 
 # def startLockpickMinigame():
+
+class Background(pygame.sprite.Sprite):
+    def __init__(self, image_file, location):
+        pygame.sprite.Sprite.__init__(self)  #call Sprite initializer
+        self.image = pygame.image.load(image_file)
+        self.rect = self.image.get_rect()
+        self.rect.left, self.rect.top = location
 
 def draw_rect_alpha(surface, color, rect):
     shape_surf = pygame.Surface(pygame.Rect(rect).size, pygame.SRCALPHA)
@@ -55,18 +75,39 @@ def draw_rect_alpha(surface, color, rect):
 
 def redrawCharMove():
     global walkCount
+    global walksfxCounter
     if walkCount + 1 >= 9:
         walkCount = 0
     if left:
+        if walksfxCounter == 10:
+            walksfxCounter = 0
+            pygame.mixer.Sound.play(charwalkongrass[random.randint(0,6)])
+        else:
+            walksfxCounter += 1
         screen.blit(charleft[walkCount//3], player_pos)
         walkCount += 1
     elif right:
+        if walksfxCounter == 10:
+            walksfxCounter = 0
+            pygame.mixer.Sound.play(charwalkongrass[random.randint(0,6)])
+        else:
+            walksfxCounter += 1   
         screen.blit(charright[walkCount//3], player_pos)
         walkCount += 1
     elif up:
+        if walksfxCounter == 10:
+            walksfxCounter = 0
+            pygame.mixer.Sound.play(charwalkongrass[random.randint(0,6)])
+        else:
+            walksfxCounter += 1
         screen.blit(charup[walkCount//3], player_pos)
         walkCount += 1
     elif down:
+        if walksfxCounter == 10:
+            walksfxCounter = 0
+            pygame.mixer.Sound.play(charwalkongrass[random.randint(0,6)])
+        else:
+            walksfxCounter += 1
         screen.blit(chardown[walkCount//3], player_pos)
         walkCount += 1
     else:
@@ -87,34 +128,39 @@ def redrawCharMove():
 while running:
     # poll for events
     # pygame.QUIT event means the user clicked X to close your window
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE and isGamePaused == False:
                 isGamePaused = True
+                pygame.mixer.Sound.play(escmenusfx)
             elif event.key == pygame.K_ESCAPE and isGamePaused == True:
                 isGamePaused = False
-        if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RETURN and txtnpcdone and txtnpcactivemsg < len(messages)-1:
-                    txtnpcactivemsg += 1
-                    done = False
-                    message = messages[txtnpcactivemsg]
-                    txtnpccounter = 0
-                elif event.key == pygame.K_RETURN and txtnpcdone and txtnpcactivemsg == len(messages)-1:
-                    txtnpcactivemsg = 0
-                    txtnpccounter = 0
-                    message = messages[txtnpcactivemsg]
-                    done = False
+                pygame.mixer.Sound.play(escmenusfx)
+            if event.key == pygame.K_RETURN and txtnpcdone and txtnpcactivemsg < len(messages)-1 and rect_char.colliderect(rect_npcreact):
+                txtnpcactivemsg += 1
+                done = False
+                message = messages[txtnpcactivemsg]
+                txtnpccounter = 0
+                pygame.mixer.Sound.play(lisatalksfx[random.randint(0,1)])
+            elif event.key == pygame.K_RETURN and txtnpcdone and txtnpcactivemsg == len(messages)-1 and rect_char.colliderect(rect_npcreact):
+                txtnpcactivemsg = 0
+                txtnpccounter = 0
+                message = messages[txtnpcactivemsg]
+                done = False
+                pygame.mixer.Sound.play(lisatalksfx[random.randint(0,1)])
 
     # fill the screen with a color to wipe away anything from last frame
+
     screen.fill("white")
+    screen.blit(bg,[0,0])
 
     clock = pygame.time.Clock()
     clock.tick(60)
     if isGamePaused == False:
         screen.blit(npc,(300,200))
-        screen.blit (door,(800,150))
         draw_rect_alpha(screen,(0, 0, 255, 0),rect_char)
         draw_rect_alpha(screen,(1,1,1, 0),rect_npc)
         draw_rect_alpha(screen,(1,1,1, 0),rect_npcreact)
@@ -139,17 +185,15 @@ while running:
             velchar = 300
 
         if rect_char.colliderect(rect_door):
-            pygame.draw.rect(screen,'black',[0 , 520, 1280, 200])
-            message = 'Lockpick denemek için Enter tuşuna bas!'
-            if txtdoorcounter < txtdoorspeed * len(message):
-                txtdoorcounter += 1
-            elif txtdoorcounter >= txtdoorspeed * len(message):
-                txtdoordone = True
-            snip = font.render(message[0:txtdoorcounter//txtdoorspeed], True, 'white')
-            screen.blit(snip, (10, 540))
-            # if keys[pygame.K_RETURN]:
-            #     startLockpickMinigame()
+            if keys[pygame.K_TAB] and songcounter == 0:
+                songcounter += 1
+                pygame.mixer.Sound.play(act0song)
+                
+                
         if rect_char.colliderect(rect_npcreact):
+            if blipcounter == 0:
+                pygame.mixer.Sound.play(lisatalksfx[random.randint(0,1)])
+                blipcounter += 1
             messages = ['Hey! Ben Lisa. Bu cümlem ise yazı motorunun test mesajı. Enter tuşuna bas!',
                        'İşte, konuşmaya devam ediyorum! ESC tuşuna basmaya ne dersin?',
                        'Adamım, harikasın ya! Tekrar Enter\'a basarsan baştan alırız.']
@@ -161,6 +205,7 @@ while running:
                 txtnpcdone = True
             snip = font.render(message[0:txtnpccounter//txtnpcspeed], True, 'white')
             screen.blit(snip, (10, 540))
+
 
         keys = pygame.key.get_pressed()
         if keys[pygame.K_w]:
@@ -203,7 +248,6 @@ while running:
         message = 'Oyun durduruldu. Devam etmek istiyorsan tekrar ESC tuşuna bas.'
         snip = font.render(message, True, 'white')
         screen.blit(snip, (30, 640/2))
-        
 
     # flip() the display to put your work on screen
     pygame.display.flip()
